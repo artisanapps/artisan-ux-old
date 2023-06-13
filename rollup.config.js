93fc42b26d187ddc85c578cf701dbb7f6c387706
Hiren <hiren@intuerisoft.com>
2023-06-13 17:31:11 -0400

    Fix terser and output file

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
@@ -33,7 +33,7 @@ export default [
     },
     {
         input: "src/index.ts",
-        output: [{ file: "dist/types.d.ts", format: "es" }],
+        output: [{ file: "dist/index.d.ts", format: "es" }],
         plugins: [dts.default()],
     },
 ];
