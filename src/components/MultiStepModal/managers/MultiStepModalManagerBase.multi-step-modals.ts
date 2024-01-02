241937f4bcf8170c31cd0ed728d48daacf5c47a2
hiren-workofartisan <hiren@workofartisan.com>
2024-01-02 14:54:17 -0500

    stash

--- src/components/MultiStepModal/managers/MultiStepModalManagerBase.multi-step-modals.ts
+++ src/components/MultiStepModal/managers/MultiStepModalManagerBase.multi-step-modals.ts
@@ -1,25 +1,43 @@
-import {makeObservable, observable} from "mobx-6";
+import {action, computed, makeObservable, observable} from "mobx-6";
 import StepManagerBase from "./StepManagerBase.multi-step-modals";
 
 abstract class MultiStepModalManagerBase {
   abstract getSteps: () => Array<StepManagerBase>;
   abstract onClose: (canceled: boolean) => void;
 
-  isOpen: boolean;
+  private _isOpen: boolean;
   disableActions: boolean;
 
   constructor() {
     this.reset();
 
-    makeObservable(this, {
+    makeObservable<MultiStepModalManagerBase, "isOpen" | "setIsOpen">(this, {
       isOpen: observable,
+      setIsOpen: action,
+      getIsOpen: computed,
       disableActions: observable,
     })
   }
 
   reset = () => {
-    this.isOpen = false;
+    this.setIsOpen(false);
     this.disableActions = false;
+  }
+
+  getIsOpen = (): boolean => {
+    return this._isOpen;
+  }
+
+  private setIsOpen = (isOpen: boolean) => {
+    this._isOpen = isOpen;
+  }
+
+  open = () => {
+    this.setIsOpen(true);
+  }
+
+  close = () => {
+    this.reset();
   }
 }
 
