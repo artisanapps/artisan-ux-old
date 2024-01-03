7e6263b0d7e4c1732e4d55ab2aeae20099ee139b
hiren-workofartisan <hiren@workofartisan.com>
2024-01-03 15:41:52 -0500

    Add controlled logic

--- src/components/Forms/FormFields/selectBoxFields/MultiSelectBoxField.tsx
+++ src/components/Forms/FormFields/selectBoxFields/MultiSelectBoxField.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {MultiSelectBoxFieldProps} from "./SelectBoxFields.types";
 import Grid from "@mui/material/Unstable_Grid2";
-import {Checkbox, FormControlLabel} from "@mui/material";
+import {Checkbox, CheckboxProps, FormControlLabel} from "@mui/material";
 
 const MultiSelectBoxField = (props: MultiSelectBoxFieldProps) => {
   const {
@@ -13,15 +13,31 @@ const MultiSelectBoxField = (props: MultiSelectBoxFieldProps) => {
       onChange
   } = props;
 
+  const onClick = (key: string) => {
+    let newValues = [...values];
+
+    if(values.includes(key)) {
+      newValues = newValues.filter(value => value !== key);
+    } else {
+      newValues.push(key);
+    }
+
+    onChange(newValues);
+  }
+
   return (
     <Grid container spacing={1}>
       {
         options.map(option => {
+          const checkboxProps: CheckboxProps = {
+            checked: values.includes(option.key),
+            onClick: () => onClick(option.key)
+          }
 
           return (
             <Grid item xs={3}>
               <FormControlLabel
-                control={<Checkbox />}
+                control={<Checkbox {...checkboxProps} />}
                 label={option.label}
               />
             </Grid>
