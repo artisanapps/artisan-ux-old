4d8afa55c709c90de22654affc382be8e112ad83
hiren-workofartisan <hiren@workofartisan.com>
2024-04-18 19:41:27 -0400

    clean up text field

--- src/components/Forms/FormFields/TextFields/TextField.tsx
+++ src/components/Forms/FormFields/TextFields/TextField.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { TextFieldProps } from "./TextField.types";
 import { TextFieldChangeEvent } from "../FormFields.types";
-import { default as MUITextField } from "@mui/material/TextField/TextField";
+import { default as MUITextField, TextFieldProps as MUITextFieldProps } from "@mui/material/TextField/TextField";
 
 const TextField = (props: TextFieldProps) => {
   const { id, value, onChange, label, multiline } = props;
@@ -9,19 +10,19 @@ const TextField = (props: TextFieldProps) => {
     onChange(event.target.value);
   };
 
-  return (
-    <MUITextField
-      id={id}
-      label={label}
-      variant={"standard"}
-      color={"primary"}
-      value={value}
-      onChange={onValueChange}
-      fullWidth={true}
-      multiline={multiline}
-      rows={2}
-    />
-  );
+  const muiTextFieldProps: MUITextFieldProps = {
+    id: id,
+    label: label,
+    variant: "standard",
+    color: "primary",
+    value: value,
+    onChange: onValueChange,
+    fullWidth: true,
+    multiline: multiline,
+    rows: 2
+  }
+
+  return <MUITextField {...muiTextFieldProps} />;
 };
 
 export default TextField;
